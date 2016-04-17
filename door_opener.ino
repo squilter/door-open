041818a7bbacd6924d1e4cfae798f91447c255d2
squilter <s.quilter@aol.com>
2016-04-16 21:19:02 -0400

    remove SPI support

--- door_opener.ino
+++ door_opener.ino
@@ -14,18 +14,8 @@
 /******************************************************************************************/
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_PN532.h>
 
-// If using the breakout with SPI, define the pins for SPI communication.
-//#define PN532_SCK  (2)
-//#define PN532_MOSI (3)
-//#define PN532_SS   (4)
-//#define PN532_MISO (5)
-//Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
 #define PN532_IRQ   (2)
 #define PN532_RESET (3)  // Not connected by default on the NFC Shield
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
